6a75289f2f1e6f2727fff5b61f296ea12f96abf5
manderj1587 <36685928+manderj1587@users.noreply.github.com>
2018-02-20 22:43:36 -0500

    Add files via upload
    first submission

---
+++ P2T1_SalesPrediction_MandersonJaya.py
@@ -0,0 +1,13 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Jaya Manderson
+# 02/18/2018
+#
+
+projectedTotalSales = float( input( "Please input projected amount" + \
+                                    " of total sales: ") )
+
+profit = 0.23 * projectedTotalSales
+
+print( "The profit is $" + format( profit , ",.2f" ) ) 
+
